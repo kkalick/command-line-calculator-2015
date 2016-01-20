21b02b7e00e9da0c021f938e9048504bc8716e91
kristy <kristen.kalick@gmail.com>
2016-01-19 20:24:03 -0500

    Update calculator.rb
    i changed some junk
    #hilarious

--- calculator.rb
+++ calculator.rb
@@ -1,22 +1,22 @@
-puts "Welcome to the Premvaree command line calculator!\n\n"
-puts "What operation would you like to use?"
+puts "Welcome to the Primavera command line calculator!\n\n"
+puts "What pasta would you like to use?"
 puts "You can add, subtract, multiply or divide."
 
 operation = gets.chomp.downcase
 
 def get_numbers
-  puts "What is the first number?"
+  puts "What is the first vegetable?"
   number1 = gets.chomp.to_i
-  puts "What is the second number?"
+  puts "What is the second vegetable?"
   number2 = gets.chomp.to_i
   return number1,number2
 end
 
 if operation == "add"
   x,y = get_numbers
-  puts "The answer is #{x+y}"
+  puts "The mushroom is #{x+y}"
 else
-  puts "That's not a valid operation."
+  puts "That's not a valid cheese."
 end
 
 
